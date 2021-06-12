d0f66b7979b1a6ccf57797ef18006ceb18dceff7
lammou <lammou@mbc.edu.mo>
2021-06-12 12:33:36 +0800

    heal code

--- routes/healthdeclaration/index.js
+++ routes/healthdeclaration/index.js
@@ -32,12 +32,17 @@ function mimetype(filename) {
 };
 
 router.get('/', (req, res, next) => {
+    let today = new Date()
+    //console.log(today.toISOString().substring(0,10))
     res.render('healthdeclaration/index.pug', {
+        curr_date: today.toISOString().substring(0,10),
         profile: req.user,
     });
 });
 /* GET home page. */
-router.use(function (req, res, next) {
+router.use('/up/:book',function (req, res, next) {
+    //console.log(req.params.book)
+    //console.log(req)
     const { headers, method, url } = req;
     let subpath = ""
     if (url[url.length - 1] == "/") { subpath = url }
@@ -45,7 +50,7 @@ router.use(function (req, res, next) {
         let l_ = url.split("/");
         for (let i = 0; i < l_.length - 1; i++) { subpath += l_[i] + "/" }
     }
-    console.log(method, url, subpath)
+    //console.log(method, url, subpath)
     let curr_path = uploadDir + subpath;
     if (req.method == 'POST') {
         //&ctx=20210607110301&crypto=18292930004302
@@ -60,65 +65,85 @@ router.use(function (req, res, next) {
                 keepExtensions: true,
                 maxFileSize: 2 * 1024 * 1024,
                 maxFields: 10,
-                maxFieldsSize: 1 * 1024 * 1024,
+                maxFieldsSize: 1  * 1024,
                 minFileSize: 1,
             }
         );
-        fileTypes = ['image/jpeg', 'image/png', 'image/gif'];
-
+        let fileTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        let today = new Date()
+        let curr_date=null;
+        let classno=null
+        let seat=null
+        let bmonth=null
+        let bday=null
+        let fieldcnt=0;
+        let part_err=null;
         form.onPart = part => {
-            if (fileTypes.indexOf(part.mime) === -1) {
+            part.on('data', (buffer) => {
+                if(part.name=="curr_date") {
+                   curr_date=buffer.toString()
+                   if(curr_date!=today.toISOString().substring(0,10)){
+                    part_err='Date Error!'
+                   }
+                }
+                if(part.name=="classno") classno=buffer.toString()
+                if(part.name=="seat") seat=buffer.toString()
+                if(part.name=="bmonth") bmonth=buffer.toString()
+                if(part.name=="bday") {
+                    bday=buffer.toString()
+                    bday=(bday.length==1?"0":"")+bday;
+                    seat=(seat.length==1?"0":"")+seat;
+                    bmonth=(bmonth.length==1?"0":"")+bmonth;
+                    bday=(bday.length==1?"0":"")+bday;
+                    let key=`${classno}${seat}${bmonth}${bday}`
+                    if(key!="SC1A050505")
+                    {
+                        part_err='學生資料有誤!'
+                    }
+
+                }
+                // do whatever you want here
+              });
+            if(part_err)
+            {
+                form._error(new Error(part_err));
+            }else if (part.filename&&fileTypes.indexOf(part.mime) === -1&& fieldcnt<5) {
                 // Here is the invalid file types will be handled. 
                 // You can listen on 'error' event
-                form._error(new Error('File type is not supported'));
+                form._error(new Error('2MB 內圖版'));
             }
-            if (!part.filename || fileTypes.indexOf(part.mime) !== -1) {
+            if(part.name=="curr_date"||part.name=="classno"||part.name=="seat"||part.name=="bmonth"||part.name=="bday"){
+                fieldcnt++;
+                form.handlePart(part);
+            }
+            else if (!part_err || !part.filename || fileTypes.indexOf(part.mime) !== -1&&fieldcnt==5) {
                 // Let formidable handle the non file-pars and valid file types
                 form.handlePart(part);
             }
         };
 
-        form.parse(request).on('error', _err => {
-            // You also pass it through next() to errorHandle function
-            debug(_err.message); // output: File type is not supported
-        })
-        form.on('field', function (field, value) {
-            console.log(field, value);
-            fields.push([field, value]);
-        })
-            /* this is where the renaming happens */
-            .on('fileBegin', function (name, file) {
-                var fileType = file.type.split('/').pop();
-                if (fileType == 'jpg' || fileType == 'png' || fileType == 'jpeg') {
-                    //rename the incoming file
-                    file.path = form.uploadDir + "/" + images_hash + '_' + image_count + '.' + fileType;
-                    //increment image counter for next possible incoming image
-                    ++image_count;
-                } else {
-                    console.log('incorrect file type: ' + fileType);
-                }
-            })
         form.parse(req, (err, fields, files) => {
             if (err) {
-                console.error(err);
+                //console.log('-----')
+                //console.error(err);
+                //console.log('-----')
                 res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
                 res.end(String(err));
                 return;
             }
-            console.log(files)
+            //console.log(files)
             let file = files.file1
             let filename = file.name.split("/")
-            filename = filename[filename.length - 1]
-            console.log(path.join(form.uploadDir, file.name), filename)
+            filename = curr_date+"_"+classno+seat+"_"+filename[filename.length - 1]
+            //console.log(path.join(form.uploadDir, file.name), filename)
             if (file) fs.promises.rename(file.path, path.join(form.uploadDir, filename));
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ fields, files }, null, 2));
+            res.end(`資料上傳成功!${filename}`)
+            //res.end(JSON.stringify({ fields, files }, null, 2));
         });
         return;
     } else {
-        res.render('healthdeclaration/index.pug', {
-            profile: req.user,
-        });
+        res.redirect("/internal/healthdeclaration")
     }
 });
 
